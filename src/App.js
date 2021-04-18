b0bd4d66c22f085ca2dc6e1db0f1e96070d61078
Sho-Taniguchi <sho.taniguchi0109@gmail.com>
2021-04-18 13:44:30 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
- import React from 'react';
- import PropTypes from 'prop-types';
+ import React, { Component } from 'react';
+//  import PropTypes from 'prop-types';
 
 // function App() {
 // 関数（ファンクショナル）コンポーネント
-const App = () => {
+// const App = () => {
+//   const profiles = [
+//     {name: "Taro", age: 10},
+//     {name: "Hanako", age: 5},
+//     {name: "NoName", age: 3}
+//   ]
+//   return (
+//     <div>
+//       {
+//         profiles.map((profiles, index) => {
+//           return <User name={profiles.name} age={profiles.age} key={index} />
+//         })
+//       }
+//     </div>
+//   )
+// }
 
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 5},
-    {name: "NoName", age: 3}
-  ]
+const App = () => (<Counter></Counter>)
 
-  return (
-    <div>
-      {
-        profiles.map((profiles, index) => {
-          return <User name={profiles.name} age={profiles.age} key={index} />
-        })
-      }
-    </div>
-  )
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    // Stateの状態を変更する際のメソッド
+    // setStateでrenderがcallbackされる
+    // 状態を変えても、その状態を表示するDOMを意識する必要がない
+    this.setState({ count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, {props.age} years old!</div>
-}
+// const User = (props) => {
+//   return <div>Hi, I am {props.name}, {props.age} years old!</div>
+// }
 
 // デフォルト値の設定
 // User.defaultProps = {
@@ -32,9 +60,9 @@ const User = (props) => {
 // }
 
 // 型チェック
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+// User.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired
+// }
 
 export default App;
